3c20f9ce4b7e5b802898728f764dcc46618f70d3
josiahkee <119419041+josiahkee@users.noreply.github.com>
2025-04-08 20:07:48 +0800

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -4,15 +4,6 @@ import { setupVite, serveStatic, log } from "./vite";
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
